7e50213af5b6e20cc2d90cd7c6ba63989b258522
Alex-Nelson-1 <113055690+Alex-Nelson-1@users.noreply.github.com>
2023-01-21 13:36:50 -0500

    Update script.js

--- js/script.js
+++ js/script.js
@@ -8,8 +8,8 @@
  * Check servie worker.
  */
 if (navigator.serviceWorker) {
-  navigator.serviceWorker.register("/ICS2O-PWA-Test/sw.js", {
-    scope: "/ICS2O-PWA-Test/",
+  navigator.serviceWorker.register("/ICS2O-UNIT6-04-JS-QUERY-STRINGS/sw.js", {
+    scope: "/ICS2O-UNIT6-04-JS-QUERY-STRINGS/",
   })
 }
 
